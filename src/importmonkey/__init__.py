0aa793d2bdcddf3ecbcc3d53e51782f9319efebf
Markus Hirsimäki <hirsimaki.markus@gmail.com>
2023-11-04 14:13:15 +0200

    Fix typo

--- src/importmonkey/__init__.py
+++ src/importmonkey/__init__.py
@@ -4,7 +4,7 @@
 
 # Updating __version__ information:
 #     * Format is <major>.<minor>.<patch>
-#     * Never use leading or trailing zeroes in any of minor/major/patch.
+#     * Never use leading or trailing zeroes in any of major/minor/patch.
 #     * A lone zero is allowed.
 #     * Increment more if an increment results in a leading or trailing zero.
 #     * Incrementing major resets minor and patch to 0.
